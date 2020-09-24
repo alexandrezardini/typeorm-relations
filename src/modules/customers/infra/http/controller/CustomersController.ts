ef558fe2e7bc08e15d53f2164fb59f839dfae37e
alexandre-roa <alexandre.z.roa@gmail.com>
2020-09-24 13:44:28 -0300

    customers migration

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,8 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    response.json({ name, email });
   }
 }
